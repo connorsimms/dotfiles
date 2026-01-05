cdf7c716f51bac7681123f80117d768538a7a676
Connor Simms <connorvsimms@gmail.com>
2026-01-05 10:50:24 -0600

    fixed nvim init path

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,11 @@
 -- Load .vimrc file
-vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
-vim.o.packpath = vim.o.runtimepath
-vim.cmd('source ~/.vimrc')
+local vimrc_path = vim.fn.expand("~/.config/vim/vimrc")
+
+if vim.fn.filereadable(vimrc_path) == 1 then
+    vim.cmd('source ' .. vimrc_path)
+else
+    print("Warning: Vim config not found at " .. vimrc_path)
+end
 
 vim.opt.termguicolors = true
 vim.g.have_nerd_font = true
