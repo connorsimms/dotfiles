8e285b4a8823cb044412fb6fb314e4795fe759bf
Connor Simms <connorvsimms@gmail.com>
2026-01-08 17:50:16 -0600

    fixed bash script

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ echo "OS: $OS"
 if [ -d "$HOME/.dotfiles" ]; then
 	echo "~.dotfiles already exists"
 	exit 1
-else
-	mkdir "$HOME/.dotfiles"
 fi
 
 DOTFILES="$HOME/.dotfiles"
@@ -17,7 +15,7 @@ if [ "$OS" == "Darwin" ]; then
 
 	if ! command -v brew &> /dev/null; then
 		echo "Homebrew not found. Installing..."
-		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh	
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	fi
 
 	if ! command -v git &> /dev/null; then
@@ -29,27 +27,27 @@ if [ "$OS" == "Darwin" ]; then
 	git clone https://github.com/connorsimms/dotfiles.git "$DOTFILES"
 
 	echo "Installing packages..."	
-	source "$DOTFILES/macos/setup.sh"
+	bash "$DOTFILES/macos/setup.sh"
 
 	echo "Configuring settings..."
-	source "$DOTFILES/macos/link.sh"
+	bash "$DOTFILES/macos/link.sh"
 
 elif [ "$OS" == "Linux" ]; then
 	echo "Running Arch Linux setup..."
 
 	if ! command -v git &> /dev/null; then
 		echo "Git not found. Installing with pacman..."
-		pacman -S git --noconfirm
+		sudo pacman -S git --noconfirm
 	fi
 	
 	echo "Cloning dotfiles..."
 	git clone https://github.com/connorsimms/dotfiles.git "$DOTFILES"
 
 	echo "Installing packages..."
-	source "$DOTFILES/arch/setup.sh"
+	bash "$DOTFILES/arch/setup.sh"
 
 	echo "Configuring settings..."
-	source "$DOTFILES/arch/link.sh"
+	bash "$DOTFILES/arch/link.sh"
 fi
 
 echo "Installation complete."
