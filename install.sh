2cddfb1870afab99e6e8d57232e5a570e1b45597
Connor Simms <connorvsimms@gmail.com>
2026-01-08 18:05:58 -0600

    macos

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ if [ "$OS" == "Darwin" ]; then
 	if ! command -v brew &> /dev/null; then
 		echo "Homebrew not found. Installing..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-        eval "$(/opt/homebrew/bin/brew shellenv)"
 	fi
+        
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
 	if ! command -v git &> /dev/null; then
 		echo "Git not found. Installing with Homebrew..."
