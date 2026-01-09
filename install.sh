7c6366b8462e7192dd5c507d8a0f231118d8eed7
Connor Simms <connorvsimms@gmail.com>
2026-01-08 23:57:11 -0600

    script

--- install.sh
+++ install.sh
@@ -3,26 +3,21 @@
 OS="$(uname -s)"
 echo "OS: $OS"
 
-if [ -d "$HOME/.dotfiles" ]; then
-	echo "~.dotfiles already exists"
-	exit 1
-fi
-
 export DOTFILES="$HOME/.dotfiles"
 
 if [ "$OS" == "Darwin" ]; then
-	echo "Running macOS setup..."
 
-	if ! command -v brew &> /dev/null; then
-		echo "Homebrew not found. Installing..."
+    if ! command xcode-select -p &> /dev/null; then
+        xcode-select --install
+        echo "Press [Enter] to continue once Xcode finishes. "
+        read
+    fi
+
+    if ! command -v brew &> /dev/null; then
+        echo "Homebrew not found. Installing..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-        if [ -f "/opt/homebrew/bin/brew" ]; then
-            eval "$(/opt/homebrew/bin/brew shellenv)"
-        elif [ -f "/usr/local/bin/brew" ]; then
-            eval "$(/usr/local/bin/brew shellenv)"
-        fi
-	fi
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
 
     brew update
 
@@ -36,11 +31,8 @@ if [ "$OS" == "Darwin" ]; then
         git clone https://github.com/connorsimms/dotfiles.git "$DOTFILES"
     fi
 
-	echo "Installing packages..."	
-	bash "$DOTFILES/macos/setup.sh"
-
-	echo "Configuring settings..."
-	bash "$DOTFILES/macos/link.sh"
+	echo "Running macOS setup..."
+	zsh "$DOTFILES/macos/setup.sh"
 
 elif [ "$OS" == "Linux" ]; then
 	echo "Running Arch Linux setup..."
@@ -49,6 +41,12 @@ elif [ "$OS" == "Linux" ]; then
 		echo "Git not found. Installing with pacman..."
 		sudo pacman -S git --noconfirm
 	fi
+
+    if [ -d "$DOTFILES" ]; then
+        git -C "$DOTFILES" pull origin main || echo "Git pull failed"
+    else
+        git clone https://github.com/connorsimms/dotfiles.git "$DOTFILES"
+    fi
 	
 	echo "Installing packages..."
 	bash "$DOTFILES/arch/setup.sh"
--- macos/setup.sh
+++ macos/setup.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
+
+export DOTFILES="$HOME/.dotfiles"
 
 brew bundle install --file="$DOTFILES/macos/Brewfile"
+
+zsh "$DOTFILES/macos/link.sh"
+
+if ! grep -q "shellenv" ~/.config/zsh/.zprofile; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.config/zsh/.zprofile
+fi
